90ecd1d63b70d980e07105cb4fe2cd9c652d0658
Ronnie <ronnie.sunde@gmail.com>
2017-07-25 21:34:44 +0200

    Add project files.

---
+++ ShortestWinter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShortestWinter
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Solution(int[] temperatures)
+        {
+            var winter = new List<int>();
+            var warmestWinter = 0;
+
+            foreach (var temp in temperatures)
+            {
+                if (winter.Count == 0)
+                {
+                    winter.Add(temp);
+                    warmestWinter = temp;
+
+                    continue; // continue on the next item in temperatures
+                }
+
+                if (temp <= warmestWinter)
+                {
+                    winter.Add(temp);
+                }
+            }
+
+            return winter.Count;
+        }
+    }
+}
